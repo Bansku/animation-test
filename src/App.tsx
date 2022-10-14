af01a94c70d51190d04ad8910f0e73c2922742bd
Annina Kivikari <annina.kivikari@nitor.com>
2022-10-14 11:55:17 +0300

    added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Lottie from 'lottie-react';
 import sustainabilityGraph from './sustainability-graph.json';
 import OPKaupunki from './OP-kaupunki.json';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 const style = {
   height: '50vh',
@@ -11,18 +13,30 @@ const style = {
 const AnimationTest = () => {
   return (
     <div className='App'>
-      <div>
+      <Typography variant='h1'>Animation tests</Typography>
+      <Box
+        sx={{
+          padding: 4,
+        }}
+      >
+        <Typography variant='h3'>Sustainability graph</Typography>
+        <Typography>Loops three times</Typography>
         <Lottie
           animationData={sustainabilityGraph}
           style={style}
           autoPlay
-          loop={false}
+          loop={2}
         />
-        ;
-      </div>
-      <div>
+      </Box>
+      <Box
+        sx={{
+          padding: 4,
+        }}
+      >
+        <Typography variant='h3'>City</Typography>
+        <Typography>Loops indefinetely</Typography>
         <Lottie animationData={OPKaupunki} style={style} />;
-      </div>
+      </Box>
     </div>
   );
 };
