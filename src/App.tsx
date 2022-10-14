7ee8c7566390cabca8159d97f52cd14f40588d35
Annina Kivikari <annina.kivikari@nitor.com>
2022-10-14 11:29:50 +0300

    Added another animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Lottie from 'lottie-react';
 import sustainabilityGraph from './sustainability-graph.json';
+import OPKaupunki from './OP-kaupunki.json';
 
-function App() {
+const style = {
+  height: '50vh',
+};
+
+const AnimationTest = () => {
   return (
     <div className='App'>
-      <Lottie animationData={sustainabilityGraph} />;
+      <div>
+        <Lottie
+          animationData={sustainabilityGraph}
+          style={style}
+          autoPlay
+          loop={false}
+        />
+        ;
+      </div>
+      <div>
+        <Lottie animationData={OPKaupunki} style={style} />;
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default AnimationTest;
